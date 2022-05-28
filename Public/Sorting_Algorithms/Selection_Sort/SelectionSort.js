82ab737c6ae0575bd10ff3585eed0cc1d4371d43
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2022-05-28 21:15:56 +0200

    Set basic set min value logic

--- Public/Sorting_Algorithms/Selection_Sort/SelectionSort.js
+++ Public/Sorting_Algorithms/Selection_Sort/SelectionSort.js
@@ -1,6 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.SelectionSort = void 0;
+var sortSwap_1 = require("../../Helpers/sortSwap");
 var SelectionSort = /** @class */ (function () {
     function SelectionSort(arrayOfNumbers) {
         this.arrayOfNumbers = arrayOfNumbers;
@@ -8,6 +9,18 @@ var SelectionSort = /** @class */ (function () {
     }
     Object.defineProperty(SelectionSort.prototype, "sortArray", {
         get: function () {
+            var arrayOfItems = this.arrayOfNumbers;
+            for (var i = 0; i < arrayOfItems.length; i++) {
+                var temporaryMinValue = 0;
+                for (var j = i + 1; j < arrayOfItems.length; j++) {
+                    if (arrayOfItems[temporaryMinValue] > arrayOfItems[j]) {
+                        temporaryMinValue = j;
+                    }
+                    if (j === arrayOfItems.length - 1) {
+                        (0, sortSwap_1.swapIndexes)(arrayOfItems, temporaryMinValue, i);
+                    }
+                }
+            }
             return this.arrayOfNumbers;
         },
         enumerable: false,
@@ -16,3 +29,5 @@ var SelectionSort = /** @class */ (function () {
     return SelectionSort;
 }());
 exports.SelectionSort = SelectionSort;
+var selection = new SelectionSort([2, 4, 5, 1, 7, 9, 2]);
+console.log(selection.sortArray);
--- src/Sorting_Algorithms/Selection_Sort/SelectionSort.ts
+++ src/Sorting_Algorithms/Selection_Sort/SelectionSort.ts
@@ -6,6 +6,25 @@ export class SelectionSort {
 	}
 
 	get sortArray() {
+		const arrayOfItems = this.arrayOfNumbers;
+
+		for (let i = 0; i < arrayOfItems.length; i++) {
+			let temporaryMinValue = 0;
+			for (let j = i + 1; j < arrayOfItems.length; j++) {
+				if (arrayOfItems[temporaryMinValue] > arrayOfItems[j]) {
+					temporaryMinValue = j;
+				}
+
+				if (j === arrayOfItems.length - 1) {
+					swapIndexes(arrayOfItems, temporaryMinValue, i);
+				}
+			}
+		}
+
 		return this.arrayOfNumbers;
 	}
 }
+
+const selection = new SelectionSort([2, 4, 5, 1, 7, 9, 2]);
+
+console.log(selection.sortArray);
