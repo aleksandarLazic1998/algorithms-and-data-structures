dd1c60ce46e8d05ad1cb9a33a15a6de92e793711
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2022-06-04 12:45:40 +0200

    Add pivot point helper function that will find point of pivot and return its value.

--- Public/Sorting_Algorithms/Quick_Sort/QuickSort.js
+++ Public/Sorting_Algorithms/Quick_Sort/QuickSort.js
@@ -3,3 +3,17 @@ var swap = function (array, firstItem, secondItem) {
     var _a;
     _a = [array[secondItem], array[firstItem]], array[firstItem] = _a[0], array[secondItem] = _a[1];
 };
+var pivotPointHelper = function (props) {
+    var array = props.array, _a = props.startIndex, startIndex = _a === void 0 ? 0 : _a, _b = props.endIndex, endIndex = _b === void 0 ? array.length : _b;
+    var pivot = array[startIndex];
+    var indexOfPivot = startIndex;
+    for (var i = startIndex + 1; i <= endIndex; i++) {
+        if (array[i] < pivot) {
+            indexOfPivot += 1;
+            swap(array, i, indexOfPivot);
+        }
+    }
+    swap(array, startIndex, indexOfPivot);
+    return indexOfPivot;
+};
+console.log(pivotPointHelper({ array: [4, 8, 2, 1, 5, 7, 6, 3] }));
--- src/Sorting_Algorithms/Quick_Sort/QuickSort.ts
+++ src/Sorting_Algorithms/Quick_Sort/QuickSort.ts
@@ -1,3 +1,28 @@
+interface IPivotHeperProps {
+	array: number[];
+	startIndex?: number;
+	endIndex?: number;
+}
+
 const swap = (array: number[], firstItem: number, secondItem: number) => {
 	[array[firstItem], array[secondItem]] = [array[secondItem], array[firstItem]];
 };
+
+const pivotPointHelper = (props: IPivotHeperProps) => {
+	const { array, startIndex = 0, endIndex = array.length } = props;
+
+	const pivot = array[startIndex];
+	let indexOfPivot = startIndex;
+
+	for (let i = startIndex + 1; i <= endIndex; i++) {
+		if (array[i] < pivot) {
+			indexOfPivot += 1;
+			swap(array, i, indexOfPivot);
+		}
+	}
+	swap(array, startIndex, indexOfPivot);
+
+	return indexOfPivot;
+};
+
+console.log(pivotPointHelper({ array: [4, 8, 2, 1, 5, 7, 6, 3] }));
