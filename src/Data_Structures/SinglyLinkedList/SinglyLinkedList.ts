b83dbad1d00050eef55f54029dc2d38dc3738c18
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2022-06-09 22:08:33 +0200

    Create Singly Linked List represent class

--- src/Data_Structures/SinglyLinkedList/SinglyLinkedList.ts
+++ src/Data_Structures/SinglyLinkedList/SinglyLinkedList.ts
@@ -1,8 +1,24 @@
+interface INode {
+	value: unknown;
+	next: unknown | null;
+}
+
 class ListNode {
 	// this.next can be of any primary or object type so it is decided in this special cas eto be unknown
-	public next: unknown;
-	constructor(public value: number) {
+	public next: unknown | null;
+	constructor(public value: unknown) {
 		this.value = value;
 		this.next = null;
+	}
+}
+
+class SignlyLinkedList {
+	public length: number;
+	public head: INode | null;
+	public tail: INode | null;
+	constructor() {
+		this.length = 0;
+		this.head = null;
+		this.tail = null;
 	}
 }
