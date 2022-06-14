77cf508440bc9800e6bf5401c530e293149209e2
aleksandarLazic1998 <aleksandar.lazic.p@gmail.com>
2022-06-14 12:07:01 +0200

    Refactor

--- Public/Data_Structures/SinglyLinkedList/SinglyLinkedList.js
+++ Public/Data_Structures/SinglyLinkedList/SinglyLinkedList.js
@@ -129,6 +129,3 @@ list.push(3);
 list.push(4);
 list.set("Aleksandar", 2);
 list.remove(3);
-/*
-    // reverse
-*/
--- src/Data_Structures/SinglyLinkedList/SinglyLinkedList.ts
+++ src/Data_Structures/SinglyLinkedList/SinglyLinkedList.ts
@@ -142,7 +142,3 @@ list.push(3);
 list.push(4);
 list.set("Aleksandar", 2);
 list.remove(3);
-
-/*
-	// reverse
-*/
